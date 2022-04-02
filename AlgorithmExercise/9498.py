693f715947d2aab5d8486f0c4efd11233dd47758
Wonyeong Park <parkwy1010@gmail.com>
2022-04-02 13:30:28 +0900

    22-04-01

---
+++ AlgorithmExercise/9498.py
@@ -0,0 +1,16 @@
+## 9498
+
+score = int(input())
+
+if score >= 90:
+    print('A')
+elif score >= 80:
+    print('B')
+elif score >= 70:
+    print('C')
+elif score >= 60:
+    print('D')
+else:
+    print('F')
+
+# 30864KB, 72ms
