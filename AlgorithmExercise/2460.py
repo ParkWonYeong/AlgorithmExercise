40f12a800881d5b68af75b51ba457b7af9e42204
Wonyeong Park <parkwy1010@gmail.com>
2022-05-13 04:51:24 +0900

    22-05-13

--- AlgorithmExercise/2460.py
+++ AlgorithmExercise/2460.py
@@ -1,6 +1,16 @@
 ## 2460
 
-a, b = map(str, input().split())
+# 기차 안에 사람이 가장 많을 때의 사람 수를 계산한다. 역은 10개이다.
+# 단, 역에서 멈추면 기차를 탈 때 내린 사람이 모두 내린 후에 기차를 탄다.
 
-# binary화 : bin(숫자형)[2:]
-print(bin(int(a,2)+int(b,2))[2:])
+riding = 0
+ans = 0
+for i in range(10):
+    exto, into = map(int, input().split())
+    riding -= exto
+    riding += into
+    ans = max(ans, riding)
+
+print(ans)
+
+# 30840KB, 72ms
