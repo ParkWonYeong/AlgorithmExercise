9c89b197b80e1bbfcc51de8dc2e2cb0e57c306bf
Wonyeong Park <parkwy1010@gmail.com>
2022-06-05 01:34:05 +0900

    22-06-05

--- AlgorithmExercise/2108.py
+++ AlgorithmExercise/2108.py
@@ -7,25 +7,31 @@
 # 4. 범위 : N개의 수들 중 최댓값과 최솟값의 차이
 
 from collections import Counter
+import sys
+input = sys.stdin.readline
 
-def modefinder(num):
+def finder(num):
     num_c = Counter(num).most_common()
     if len(num_c) > 1:
         if num_c[0][1] == num_c[1][1]:
-            print(num_c[1][0])
+            return num_c[1][0]
         else:
-            print(num_c[0][0])
+            return num_c[0][0]
     else:
-        print(num_c[0][0])    
+        return num_c[0][0] 
 
+## main ##
 N = int(input())
 num = []
 
 for i in range(N):
     num.append(int(input()))
+num.sort()
 
 print(round(sum(num)/N))    # 산술평균 출력
-num.sort()
 print(num[N//2])            # 중앙값 출력
-print(modefinder(num))      # 최빈값 출력
+print(finder(num))          # 최빈값 출력
 print(num[-1]-num[0])       # 범위 출력
+
+# 52260KB, 532ms
+# 함수를 쓰지 않고 바로 Counter를 사용하는 것이 더 작은 메모리로 빠르게 처리할 수 있었음.
