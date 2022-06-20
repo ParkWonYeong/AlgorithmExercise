db7a69c030e9ee33277f993af9a882afe3d9c1e4
Wonyeong Park <parkwy1010@gmail.com>
2022-06-20 10:05:11 +0900

    22-06-19

--- AlgorithmExercise/2407.py
+++ AlgorithmExercise/2407.py
@@ -1,7 +1,6 @@
 ## 2407
 
 n,m = map(int, input().split())
-
 ans = 1
 for i in range(n):
     ans *= i+1
@@ -9,7 +8,6 @@ for j in range(m):
     ans //= j+1
 for k in range(n-m):
     ans //= k+1
-
 print(ans)
 
 # 30840KB, 68ms
