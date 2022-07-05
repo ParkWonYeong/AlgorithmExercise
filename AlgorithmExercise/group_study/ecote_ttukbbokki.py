8e5e592fae620f227a5243b24b9100e78ec8425f
Wonyeong Park <parkwy1010@gmail.com>
2022-07-05 22:19:59 +0900

    22-07-05

--- AlgorithmExercise/group_study/ecote_ttukbbokki.py
+++ AlgorithmExercise/group_study/ecote_ttukbbokki.py
@@ -11,15 +11,18 @@ rice = list(map(int, input().split()))
 
 # 요청한 총 길이가 m일 때 적어도 m만큼의 떡을 얻기 위해
 # 절단기에 설정할 수 있는 "높이"의 최댓값은?
-start, end = 0, len(rice)-1
+start, end = 0, max(rice)
 
 while (start <= end):
     mid = (start+end)//2
-    total = 0
+    total = 0               # 목표치
     
     for i in rice:
         if i >= mid:
             total += i-mid  # 각각 잘린 떡의 길이 더하기
+
+    # 위는 다음 한 줄로도 표현 가능하다.
+    # total = sum(i-mid if i>=mid else 0 for i in rice)
     
     if total >= m:  # 목표치보다 높거나 같은 경우
         start = mid+1
