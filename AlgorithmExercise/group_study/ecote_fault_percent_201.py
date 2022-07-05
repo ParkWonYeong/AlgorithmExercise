abcfcba389f3d743964fff2ba04da021ff727df1
ParkWonYeong <parkwy1010@gmail.com>
2022-07-05 16:43:58 +0900

    22-07-05

--- AlgorithmExercise/group_study/ecote_fault_percent_201.py
+++ AlgorithmExercise/group_study/ecote_fault_percent_201.py
@@ -1,3 +1,29 @@
 ## 떡볶이 떡 만들기
 
+import sys
+input = sys.stdin.readline
 
+# 떡의 개수 n, 요청한 떡의 길이 m
+n,m = map(int, input().split())
+
+# 떡의 개별 높이(m보다 작은 길이는 잘리지 않고, 긴 떡은 잘린다)
+rice = list(map(int, input().split()))
+
+# 요청한 총 길이가 m일 때 적어도 m만큼의 떡을 얻기 위해
+# 절단기에 설정할 수 있는 "높이"의 최댓값은?
+start, end = 0, len(rice)-1
+
+while (start <= end):
+    mid = (start+end)//2
+    total = 0
+    
+    for i in rice:
+        if i >= mid:
+            total += i-mid  # 각각 잘린 떡의 길이 더하기
+    
+    if total >= m:  # 목표치보다 높거나 같은 경우
+        start = mid+1
+    else:           # 목표치보다 낮은 경우
+        end = mid-1
+
+print(end)
