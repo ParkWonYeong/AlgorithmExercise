9615acb23e85a162c506739be572bfaf5984dd47
ParkWonYeong <parkwy1010@gmail.com>
2022-08-02 20:08:02 +0900

    22-08-02

--- AlgorithmExercise/18405.py
+++ AlgorithmExercise/18405.py
@@ -2,8 +2,9 @@
 ## 경쟁적 전염
 
 # NxN 크기의 시험관
-# 모든 바이러스는 1번부터 K번까지 바이러스 종류 중 하나
+# 모든 바이러스는 1번부터 K번까지 바이러스 종류 중 하나,
 # 1초마다 상하좌우 방향으로 증식, 번호가 낮은 종류의 바이러스부터 먼저 증식
+# => 오름차순으로 정렬하여 낮은 번호부터 전염되는 조건을 확인해서 풀어나간다.
 
 import sys
 from collections import deque
@@ -12,42 +13,42 @@ input = sys.stdin.readline
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
-ans = 0
-
-def bfs(s, virus):
-    global cnt_sec
-
-    for i in range(4):  # 상하좌우 탐색
-        nx = x + dx[i]
-        ny = y + dy[i]
-
-        if 0<=nx<n and 0<=ny<n:
-            if graph[nx][ny] == 0:  # 비어있는 경우에만
-                return ###
-    q = deque([])
-
-    while q:
-
-    
-
-    #####
-    #####
-
-    return
-
-
-
-
-### main ###
-
 n,k = map(int, input().split()) # N: 시험관 크기, K: 바이러스 종류 수
+graph = []      # 시험관
+virus_info = [] # 0이 아닌 바이러스에 대한 정보
 
 # 시험관의 정보 받기
-graph = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+    # 간선 연결
+    for j in range(n):
+        if graph[i][j] != 0:
+            virus_info.append((graph[i][j], i, j, 0))   # 바이러스 숫자, 위치(x,y), 시간(초)
+
+virus_info.sort()       # 작은 숫자의 종류부터 증식시키기 위해 오름차순으로 정렬
+
+queue = deque(virus_info)
 
 # S, X, Y
 s,x,y, = map(int, input().split())      # S: 초, X,Y: 각각 행과 열의 위치
 
-cnt_sec = 0
-# 바이러스 증식(너비 우선 탐색)
-bfs(s, virus)
+
+### BFS ###
+
+while queue:
+    virus, a, b, time = queue.popleft()     # 바이러스 숫자, 위치(x,y), 시간(초) 뽑아내기
+
+    if time == s:       # 알고자 하는 시간 s초가 되면 중지
+        break
+
+    for i in range(4):  # 상하좌우 탐색
+        nx = a + dx[i]
+        ny = b + dy[i]
+
+        if 0<=nx<n and 0<=ny<n:
+            if graph[nx][ny] == 0:      # 바이러스가 확산될 수 있는 위치라면
+                graph[nx][ny] = virus   # 동일한 바이러스로 감염시킨다.
+                queue.append((virus, nx, ny, time+1))   # 1초 증가시키고 다음으로 이동
+
+print(graph[x-1][y-1])
---
+++ AlgorithmExercise/1932.py
@@ -0,0 +1,68 @@
+## 1932
+## 정수삼각형
+
+# 위층부터 시작해서 아래 있는 수(대각선 왼쪽 또는 오른쪽)
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())        # 1<=n<=500
+
+# 자기자신 인덱스 또는 +1번째 인덱스와 합할 수 있다.
+triangle = []
+
+for _ in range(n):
+    triangle.append(list(map(int, input().split())))
+
+# 위에서부터 차례로 더해내려간다.
+for i in range(1, n):
+
+    for j in range(len(triangle[i])):
+        
+        # 왼쪽 끝인 경우 본인 인덱스만 더할 수 있다.
+        if j == 0:
+            triangle[i][j] += triangle[i-1][j]
+
+        # 오른쪽 끝인 경우 +1 인덱스만 더할 수 있다.
+        elif j == len(triangle[i])-1:
+            triangle[i][j] += triangle[i-1][j-1]
+
+        else:
+            triangle[i][j] = max(triangle[i-1][j-1]+triangle[i][j] , triangle[i-1][j]+triangle[i][j])
+ 
+print(max(triangle[-1]))
+
+
+### IDEA: 더해내려가는 방식(탑다운)
+
+# 7
+# 3 8
+# 8 1 0
+# 2 7 4 4
+# 4 5 2 6 5
+
+# 7
+# 10 15
+# 8 1 0
+# 2 7 4 4
+# 4 5 2 6 5
+
+# 7
+# 10 15
+# 18 16 15
+# 2 7 4 4
+# 4 5 2 6 5
+
+# 7
+# 10 15
+# 18 16 15
+# 20 25 20 19
+# 4 5 2 6 5
+
+# 7
+# 10 15
+# 18 16 15
+# 20 25 20 20
+# 24 30 27 26 24
+
+# max(마지막 줄) = 30
