73d9ec2ceeb7fdb5d62f3f05833d2d0b2c322457
ParkWonYeong <parkwy1010@gmail.com>
2022-08-10 18:32:55 +0900

    22-08-10

---
+++ AlgorithmExercise/programmers/12947.py
@@ -0,0 +1,15 @@
+## 12947
+
+def solution(x):
+    check_x = str(x)
+    
+    cnt_x = 0
+    for i in range(len(check_x)):
+        cnt_x += int(check_x[i])
+    
+    answer = False
+    
+    if x % cnt_x == 0:
+        answer = True
+    
+    return answer
