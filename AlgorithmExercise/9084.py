11062e82cfe43c290558481b2c720b4a5f3fa2cd
DrunkRabbit <67958557+ParkWonYeong@users.noreply.github.com>
2022-08-23 23:51:41 +0900

    22-08-23

--- AlgorithmExercise/9084.py
+++ AlgorithmExercise/9084.py
@@ -4,13 +4,15 @@ import sys
 
 for _ in range(3):
     cnt = 0
+
     T = int(sys.stdin.readline())       # 테스트 케이스 개수
     N = int(sys.stdin.readline())       # 동전 N가지
+    
     coin = list(map(int, sys.stdin.readline().split()))
     money = int(sys.stdin.readline())
+
     for i in coin:
         if money % coin == 0:
             cnt += 1
-
 
     print(cnt)
