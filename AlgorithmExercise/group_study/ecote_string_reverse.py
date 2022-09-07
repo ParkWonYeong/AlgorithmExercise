9d595a0b133fc20c499bddacf56d0b199d0d54a6
Wonyeong Park <parkwy1010@gmail.com>
2022-09-07 23:08:09 +0900

    22-09-07

--- AlgorithmExercise/group_study/ecote_string_reverse.py
+++ AlgorithmExercise/group_study/ecote_string_reverse.py
@@ -8,3 +8,16 @@ for i in range(len(s)-1):
         cnt += 1
 
 print(cnt//2)
+
+
+
+## 문자열 뒤집기(2회독)
+
+s = input() # 문자열 받기
+cnt = 1
+
+for i in range(len(s)-1):
+    if s[i] != s[i+1]:      # 다른 숫자가 나올때마다 count
+        cnt += 1
+
+print(cnt//2)
