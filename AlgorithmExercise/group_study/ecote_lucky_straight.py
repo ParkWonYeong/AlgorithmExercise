85664239791f5af8d1b1ded9cabe8fe5fd4d32c9
Wonyeong Park <parkwy1010@gmail.com>
2022-09-12 23:06:27 +0900

    22-09-12

--- AlgorithmExercise/group_study/ecote_lucky_straight.py
+++ AlgorithmExercise/group_study/ecote_lucky_straight.py
@@ -18,3 +18,21 @@ if ans1 == ans2:
     print("LUCKY")
 else:
     print("READY")
+
+
+## 럭키 스트레이트(2회독)
+
+n = input()
+mid = len(n)//2
+
+score_a,score_b = n[:mid],n[mid:]
+a,b = 0,0
+
+for i in range(mid):
+    a += int(score_a[i])
+    b += int(score_b[i])
+
+if a == b:
+    print("LUCKY")
+else:
+    print("READY")
