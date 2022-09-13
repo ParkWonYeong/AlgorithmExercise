35705a29aae0f295f918c39b5a83062b8d8cac85
Wonyeong Park <parkwy1010@gmail.com>
2022-09-13 23:57:24 +0900

    22-09-13

--- AlgorithmExercise/group_study/ecote_string_resort.py
+++ AlgorithmExercise/group_study/ecote_string_resort.py
@@ -3,18 +3,37 @@
 # 알파벳 대문자와 숫자로만 구성된 문자열.
 # 알파벳을 모두 오름차순 한 뒤에 숫자 정렬.
 
+# s = input()
+# alpha=''
+# num=0
+
+# for i in range(len(s)):
+#     if s[i].isalpha() == True:  #  알파벳이면 alpha string에 추가
+#         alpha += s[i]
+#     else:
+#         num += int(s[i])        # 숫자면 num에 더하기
+
+# alpha = sorted(alpha)           # 오름차순 정렬
+# alpha += str(num)               # 모든 숫자를 더한 값 num 추가
+
+# for i in range(len(alpha)):
+#     print(alpha[i], end='')
+
+
+## 문자열 재정렬(2회독)
+
 s = input()
-alpha=''
-num=0
-
-for i in range(len(s)):
-    if s[i].isalpha() == True:  #  알파벳이면 alpha string에 추가
-        alpha += s[i]
+alpha, num, ans = '', 0, ''
+for i in s:
+    if i.isalpha() == True:
+        alpha += i
     else:
-        num += int(s[i])        # 숫자면 num에 더하기
+        num += int(i)
 
-alpha = sorted(alpha)           # 오름차순 정렬
-alpha += str(num)               # 모든 숫자를 더한 값 num 추가
+alpha = sorted(alpha)
 
-for i in range(len(alpha)):
-    print(alpha[i], end='')
+for i in alpha:
+    ans += i
+ans += str(num)
+
+print(ans)
