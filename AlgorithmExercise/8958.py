d7e1c73832c86778453b88b981f49dc708a7c088
Wonyeong Park <parkwy1010@gmail.com>
2022-09-21 23:59:32 +0900

    22-09-21

--- AlgorithmExercise/8958.py
+++ AlgorithmExercise/8958.py
@@ -3,18 +3,17 @@
 n = int(input())    # 테스트 케이스 개수
 
 for i in range(n):
+
     quiz = input()
-    score, cnt = 0,1
+    score, cnt = 0,0
+
     for j in range(len(quiz)):
-        
+
         if quiz[j] == 'O':
+            cnt += 1
             score += cnt
 
-        if j < (len(quiz)-1):
-            if quiz[j+1] != 'O':
-                cnt = 0
-            else:
-                cnt += 1
         else:
-            cnt += 1
+            cnt = 0
+
     print(score)
