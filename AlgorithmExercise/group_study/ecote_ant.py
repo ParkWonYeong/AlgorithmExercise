b9f6f494445e41c4ec28bb4ba206c5440d148a6a
Wonyeong Park <parkwy1010@gmail.com>
2022-11-05 23:58:38 +0900

    22-11-05

--- AlgorithmExercise/group_study/ecote_ant.py
+++ AlgorithmExercise/group_study/ecote_ant.py
@@ -15,6 +15,21 @@ for idx in range(1, n):
     # print('idx: ', idx)
     # print('dp[idx-1]:',dp[idx-1])
     # print('eat[idx]:', eat[idx])
+    
     dp[idx] = max(dp[idx-1], dp[idx-2]+eat[idx])
+
+print(dp[n-1])
+
+
+### 개미병사(2회독)
+
+n = int(input())    # 식량창고의 개수(3<=N<=100)
+eat = list(map(int, input().split()))     # 각 식량창고에 저장된 식량 개수(0<=K<=1000)
+
+dp = [0]*100
+dp[1] = max(eat[0], eat[1])
+
+for i in range(1, n):
+    dp[i] = max(dp[i-1], dp[i-2]+eat[i])
 
 print(dp[n-1])
